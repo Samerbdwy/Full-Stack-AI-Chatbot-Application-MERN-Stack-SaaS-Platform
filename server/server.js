a20fb4e3564be927cd4c164daec5c257e64cdba5
Samer <sa.badawy2005@gmail.com>
2025-10-06 00:04:32 +0300

    fixes

--- server/server.js
+++ server/server.js
@@ -65,5 +65,5 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
